af07ad47991223cbedd1b039795e6232e4eef98f
sherkoo <stephenherko@gmail.com>
2019-08-10 00:21:03 -0500

    Create client with vue
    - Front-end powered by vue
    - Building out routes for search
    - Constructing homepage
    - Minor functionality with player search

---
+++ client/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <header>
+    <img :src="image">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data(){
+    return{
+      image: require('@/assets/logo.png')
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/Search.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <section>
+    <h1>Track Player Stats</h1>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="platform">Platform</label>
+        <select id="platform" name="platform" v-model="platform">
+          <option value="psn">Playstation</option>
+          <option value="xbl">XBOX Live</option>
+          <option value="Origin">Origin</option>
+        </select>
+        <br>
+        <br>
+        <input v-model="gamertag" id="gamertag" type="text" name="text" placeholder="origin id, xbox live name">
+        <input type="submit" value="submit">
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data(){
+    return{
+      platform: "psn",
+      gamertag: ""
+    }
+  },
+  beforeCreate(){
+    document.body.className = "body-bg-image";
+  },
+  methods: {
+    onSubmit(){
+      if(!this.gamertag){
+        console.log('gamertag not found');
+      } else {
+        // console.log('redirect');
+        this.$router.push(`/profile/${this.platform}/${this.gamertag}`);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
